81d4d58b64d9115fa8fe5af01b5c4428851ca938
[dyad] <Theivsightcompany@gmail.com>
2026-01-22 13:26:22 -0500

    [dyad] Adding confetti effect for task completion - wrote 7 file(s), added react-confetti, @types/react-confetti package(s)

---
+++ src/components/Confetti.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactConfetti from 'react-confetti';
+
+interface ConfettiProps {
+  onComplete: () => void;
+}
+
+export const Confetti: React.FC<ConfettiProps> = ({ onComplete }) => {
+  const [dimensions, setDimensions] = React.useState({ width: 0, height: 0 });
+
+  React.useEffect(() => {
+    // Set dimensions on mount
+    const { innerWidth: width, innerHeight: height } = window;
+    setDimensions({ width, height });
+  }, []);
+
+  if (dimensions.width === 0) {
+    return null;
+  }
+
+  return (
+    <ReactConfetti
+      width={dimensions.width}
+      height={dimensions.height}
+      recycle={false}
+      numberOfPieces={500}
+      gravity={0.15}
+      onConfettiComplete={(confetti) => {
+        onComplete();
+        if (confetti) {
+          confetti.clear();
+        }
+      }}
+      style={{ position: 'fixed', top: 0, left: 0, zIndex: 1000 }}
+    />
+  );
+};
